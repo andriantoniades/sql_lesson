20f50c2823ea6233e66c1ef315c5af968c6ca36c
mike antoniades <mikeantoniades@andri-antoniades-MacBook-2.local>
2014-03-06 12:51:20 -0800

    hb_app working, syncing repos

--- hackbright_app.py
+++ hackbright_app.py
@@ -3,32 +3,76 @@ import sqlite3
 DB = None
 CONN = None
 
-def get_student_by_github(github):
-    query = """SELECT first_name, last_name, github FROM Students WHERE github = ?"""
-    DB.execute(query, (github,))
-    row = DB.fetchone()
-    print """\
-Student: %s %s
-Github account: %s"""%(row[0], row[1], row[2])
-
 def connect_to_db():
     global DB, CONN
     CONN = sqlite3.connect("hackbright.db")
     DB = CONN.cursor()
 
+
+def make_new_student(first_name, last_name, github):
+    query = """INSERT into Students values (?, ?, ?)"""
+    DB.execute(query, (first_name, last_name, github))
+    CONN.commit()
+    
+    return "Successfully added student: %s %s" % (first_name, last_name)
+
+def make_new_project(title, description, max_grade):
+    query = """INSERT into Projects (title, description, max_grade) values (?, ?, ?)"""
+    DB.execute(query, (title, description, max_grade))
+    CONN.commit()
+    return "Successfully added project: %s" % title
+
+def make_new_grade(title, description, max_grade):
+    query = """INSERT into Grades values (?, ?, ?)"""
+    DB.execute(query, (title, description, max_grade))
+
+    CONN.commit()
+    return "Successfully added grade: %d" % max_grade
+
+def get_student_by_github(github):
+    query = """SELECT first_name, last_name, github FROM Students WHERE github = ?"""
+    DB.execute(query, (github,))
+    row = DB.fetchone()
+    return """\
+Student: %s %s
+Github account: %s"""%(row[0], row[1], row[2])
+    
+
+def get_project_by_title(title):
+    query = """SELECT title, description, max_grade FROM Projects WHERE title = ?"""
+    DB.execute(query, (title,))
+    row = DB.fetchone()
+    return row
+    
+   
+def get_grade_from_grades(max_grade):
+    query = """SELECT title, description, max_grade FROM Grades WHERE max_grade = ?"""
+    DB.execute(query, (max_grade,))
+    row = DB.fetchone()
+    return row
+    
+
 def main():
     connect_to_db()
     command = None
     while command != "quit":
-        input_string = raw_input("HBA Database> ")
+        input_string = raw_input("Your Python App That Accesses The Database> ")
         tokens = input_string.split()
         command = tokens[0]
         args = tokens[1:]
 
         if command == "student":
-            get_student_by_github(*args) 
+            print get_student_by_github(*args) 
         elif command == "new_student":
-            make_new_student(*args)
+            print make_new_student(*args)
+        elif command == "project":
+            print get_project_by_title(*args)
+        elif command == "new_project":
+            print make_new_project(*args)
+        elif command == "max_grade":
+            print get_grade_from_grades(*args)
+        elif command == "make_new_grade":
+            print make_new_grade(*args)
 
     CONN.close()
 
